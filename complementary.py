5f8f3a4936609619fb06c286dd58e49b413e8ccf
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-10-09 11:26:05 +0300

    added to docstring in comp.py

--- complementary.py
+++ complementary.py
@@ -1,7 +1,7 @@
 def DNA_strand(dna):
     """This function iterates through the DNA strain
     given and changes the letters as prescribed.
-    It is a challenge found in codewars. The link is in the read me."""
+    It is a challenge found in codewars. The link is in the read me found in this repo."""
     new_string = ''
     for letter in dna:
         if letter == "A":
